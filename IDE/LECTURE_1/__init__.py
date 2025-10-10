7826fcdd3ba0dccaa0f38bce4e17745cb61f0645
ayemorada11-debug <ayemorada11@gmail.com>
2025-10-10 14:07:56 +0800

    Python

---
+++ IDE/LECTURE_1/__init__.py
@@ -0,0 +1,11 @@
+#string
+print("hello world")
+print("hello world")
+print("hello world")
+print(' Drei said : "nagkaon ka na lab" ' )
+print(" Drei said : \nagkaon ka na lab \"" )
+input("Enter your name : " )
+input("Enter your age : " )
+input('stop the car')
+input('boundary na')
+
