8263a44a00678427a51f439cfb31ea8db71b6a75
ayemorada11-debug <ayemorada11@gmail.com>
2025-10-27 23:16:54 +0800

    Week 2

---
+++ IDE W5/Math Operations.py
@@ -0,0 +1,36 @@
+from os import rename
+
+A = 20
+B = 30
+C = 40
+FloatA = 2.5
+FloatB = 3.5
+ComplexA = 25-31j
+ComplexB=10-12j
+
+# +, -, /, *
+SUM = A + B
+print(SUM)
+
+Diff = A - B
+print (Diff)
+
+Prod =  A*B
+print (Prod)
+
+Quotient =  A / B
+print (Quotient)
+
+remainder = A % C #modulu
+print(remainder)
+
+remainder =  A % B #modulu
+print(remainder)
+
+#Parenthesis, Expo, Multi, Divi, Add, Sub
+ComProduct = ComplexA * ComplexB
+print(ComProduct)
+
+ADD = (A / B) + (FloatA / FloatB)
+print(ADD)
+
