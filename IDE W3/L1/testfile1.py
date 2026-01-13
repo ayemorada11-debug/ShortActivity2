8312627f9d27aa4f96286b7117a6c8b17d95eb80
ayemorada11-debug <ayemorada11@gmail.com>
2026-01-13 21:29:01 +0800

    Week 3

---
+++ IDE W3/L1/testfile1.py
@@ -0,0 +1,40 @@
+#STRING
+print ("hello world")
+print ('hello world')
+print ("Drei said: sup world")
+print ("Drei said: \"sup world\"")
+input ("Enter your name: ")
+input ("Enter your age: ")
+
+#number (integer, float, long, double)
+MyGreetings = "sup baby"
+Age = 30
+
+print (MyGreetings)
+print (Age)
+
+#str
+#string
+#number (integer,float,long,double)
+
+myGreetings = "HELLO WORLD"
+myAge = 25
+
+combineGreetAndAge1 = "HELLO WORLD" + str(25)
+combineGreetAndAge2 =  myGreetings + str(myAge)
+
+print(combineGreetAndAge1)
+print(combineGreetAndAge2)
+
+
+#string
+#number (integer,float,long,double)
+
+a = input("Enter first number")
+b = input("Enter second number")
+
+sumString = a + b
+sumInt = int(a) + int(b)
+
+print(sumString)
+print(sumString)
